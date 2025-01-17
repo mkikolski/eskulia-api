3ef964a4984dbf2cd6d52adbac9ae7fe8ed6289c
NataliaKowal <nataliakowal328@gmail.com>
2025-01-17 18:32:59 +0100

    added mock chatbot endpoint

---
+++ chatbot/views.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+import firebase_admin
+from django.shortcuts import render
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1 import ArrayUnion
+from google.protobuf.internal.well_known_types import Timestamp
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from random import choice
+from django.conf import settings
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(settings.FIREBASE_CONFIG)
+    firebase_admin.initialize_app(cred)
+
+# Get Firestore client
+db = firestore.client()
+
+class MockBotEndpoint(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        doc_id = request.data.get('doc_id')
+        user_msg = request.data.get('user_msg')
+
+        if not doc_id or not user_msg:
+            return Response({"error": "Missing doc_id or user_msg"}, status=status.HTTP_400_BAD_REQUEST)
+
+        resp = choice([
+            "She's an emotion avenger",
+            "She is the villain who sends a",
+            "Line of dark fantastic passion",
+            "Words of love",
+            "Words so leisured",
+            "Words are poisoned darts of pleasure",
+            "Die"
+        ])
+
+        _, msg_ref = db.collection(u'messages').add({"id": "", "message": user_msg, "sentByBot": False, "timestamp": Timestamp().FromDatetime(datetime.now())})
+        db.collection(u'messages').document(msg_ref.id).update({"id": msg_ref.id})
+
+        _, msg_ref_bot = db.collection(u'messages').add(
+            {"id": "", "message": resp, "sentByBot": True, "timestamp": Timestamp().FromDatetime(datetime.now())})
+        db.collection(u'messages').document(msg_ref_bot.id).update({"id": msg_ref_bot.id})
+
+        _, chat_ref = db.collection(u'chats').document(doc_id).update({"chatMessageIds": ArrayUnion([msg_ref.id, msg_ref_bot.id]), "lastMessage": resp})
+
+        return Response(status=status.HTTP_200_OK)
