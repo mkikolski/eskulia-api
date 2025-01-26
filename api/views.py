d393c7fe843d9424d15a917d87e2ef0d566ea7de
Natalia Kowal <nataliakowal328@gmail.com>
2025-01-26 17:36:49 +0100

    aded draft of notifications handler

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
+def notifications_handler(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            print(data)
+            # parsowanie ustawień wysłanych przez POST request
+            # ...
+
+            # aktualizacja ustawień w db
+            # ...
+            
+            return JsonResponse({
+                'status': 'success',
+            })
+        except json.JSONDecodeError:
+            return JsonResponse({
+                'status': 'error',
+                'message': 'Invalid JSON format'
+            }, status=400)
+    
+    return JsonResponse({
+        'status': 'error',
+        'message': 'Method not allowed'
+    }, status=405)
